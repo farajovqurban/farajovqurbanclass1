e891dba02592b1d9e4540d3a24c0323badf87d8e
Farajov Qurban <farajovqurban@users.noreply.github.com>
2019-10-17 09:55:28 +0400

    try gitignore idea

--- src/main/java/classtask1.java
+++ src/main/java/classtask1.java
@@ -1,5 +1,16 @@
-public class classtask1 {
+import java.lang.*;
 
+public class classtask1
+{
+    public static void main(String[] args)
+    {
+        int num;
+        for(num=0; num < 10; num++)
+        {
+           //int num = 5;
+           System.out.println(num);
+        }
 
+    }
 
 }
